06230c7b7be304a652be2ed186ef6c1db76f6bdc
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-11-26 11:40:37 +0530

    Added tests for utils.js

---
+++ test/testUtils.js
@@ -0,0 +1,85 @@
+const assert = require("assert");
+const pairUserEnteredValues = require("../src/utils.js").pairUserEnteredValues;
+const getBeveragesData = require("../src/utils.js").getBeveragesData;
+const getAsMessage = require("../src/utils.js").getAsMessage;
+
+describe("pairUserEnteredValues", function() {
+  it("should return a paired object for one pair", function() {
+    const userEnteredValues = ["--empId", "11111"];
+    const expected = { "--empId": "11111" };
+    assert.deepStrictEqual(pairUserEnteredValues(userEnteredValues), expected);
+  });
+  it("should return a paired object for more than one pair", function() {
+    const userEnteredValues = [
+      "--beverage",
+      "orange",
+      "--empId",
+      "11111",
+      "--qty",
+      "1"
+    ];
+    const expected = {
+      "--beverage": "orange",
+      "--empId": "11111",
+      "--qty": "1"
+    };
+    assert.deepStrictEqual(pairUserEnteredValues(userEnteredValues), expected);
+  });
+});
+
+describe("getBeveragesData", function() {
+  it("should read the given string as object", function() {
+    const read = function(path) {
+      return path;
+    };
+    const data =
+      '{"25323":[{"empId":"25323","beverage":"orange","qty":"1","date":"2019-11-25T18:27:52.164Z"}]}';
+    const expected = {
+      "25323": [
+        {
+          empId: "25323",
+          beverage: "orange",
+          qty: "1",
+          date: "2019-11-25T18:27:52.164Z"
+        }
+      ]
+    };
+    assert.deepStrictEqual(getBeveragesData(read, data), expected);
+  });
+});
+
+describe("getAsMessage", function() {
+  it("should return a string for transaction details for save command", function() {
+    const transactionDetails = [
+      "Transaction Recorded:\nEmployee ID,Beverage,Quantity,date\n",
+      [
+        {
+          empId: "25323",
+          beverage: "orange",
+          qty: 1,
+          date: "2019-11-25T18:27:52.164Z"
+        }
+      ]
+    ];
+    const expected =
+      "Transaction Recorded:\nEmployee ID,Beverage,Quantity,date\n25323,orange,1,2019-11-25T18:27:52.164Z";
+    assert.equal(getAsMessage(transactionDetails), expected);
+  });
+  it("should return a string for transaction details for query command", function() {
+    const transactionDetails = [
+      "Employee ID,Beverage,Quantity,date\n",
+      [
+        {
+          empId: "25323",
+          beverage: "orange",
+          qty: 1,
+          date: "2019-11-25T18:27:52.164Z"
+        }
+      ],
+      "\nTotal:1"
+    ];
+    const expected =
+      "Employee ID,Beverage,Quantity,date\n25323,orange,1,2019-11-25T18:27:52.164Z\nTotal:1";
+    assert.equal(getAsMessage(transactionDetails), expected);
+  });
+});
